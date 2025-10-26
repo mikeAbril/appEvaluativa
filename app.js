c2c98f7455bfc9281296b6390f91dc87eca60930
mikeAbril <jaimecarreno734@gmail.com>
2025-10-26 10:29:47 -0500

    primer comit

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from "express"
+import 'dotenv/config'
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, ()=> console.log(`servidor corriendo en http://localhost:${process.env.PORT}`));
