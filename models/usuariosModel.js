5a9c2d10d344d9dc45b85a5bc2364be5e26c20f5
deivicontreras <deivibogoya@gmail.com>
2025-11-10 22:24:29 -0500

    r

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -1,27 +1,41 @@
 import pool from "../config/mysql.js";
 
 export async function obtenerUsuarios() {
-    const [rows] = await pool.query('SELECT * FROM usuarios');
-    return rows;
+  const [rows] = await pool.query("SELECT * FROM usuarios");
+  return rows;
 }
 
-export async function obtenerUsuarioPorId(id){
-    const [rows] = await pool.query('SELECT * FROM usuarios WHERE id =?',[id]);
-    return rows[0];
+export async function obtenerUsuarioPorId(id) {
+  const [rows] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [id]);
+  return rows[0];
 }
 
-export async function crearUsuario(nombre, email, fecha_nacimiento){
-    const [result] = await pool.query(
-    'INSERT INTO usuarios (nombre, email, fecha_nacimiento) VALUES (?, ?, ?)',[nombre, email, fecha_nacimiento]);
-    return { id: result.insertId, nombre, email, fecha_nacimiento };
+export async function crearUsuario(nombre, email, fecha_nacimiento, estado = "inactivo") {
+  const [result] = await pool.query(
+    "INSERT INTO usuarios (nombre, email, fecha_nacimiento, estado) VALUES (?, ?, ?, ?)",
+    [nombre, email, fecha_nacimiento, estado]
+  );
+  return { id: result.insertId, nombre, email, fecha_nacimiento, estado };
 }
 
-export async function actualizarUsuario(id,{nombre, email}){
-    await pool.query('UPDATE usuarios SET nombre = ?, email = ? WHERE id = ?', [nombre, email, id]);
-    return { id, nombre, email };
+export async function actualizarUsuario(id, data) {
+  const { nombre, email, fecha_nacimiento, estado } = data;
+
+  await pool.query(
+    "UPDATE usuarios SET nombre = ?, email = ?, fecha_nacimiento = ?, estado = ? WHERE id = ?",
+    [nombre, email, fecha_nacimiento, estado, id]
+  );
+
+  return { id, ...data };
 }
 
-export async function eliminarUsuario(id){
-    await pool.query('DELETE FROM usuarios WHERE id = ?', [id]);
-    return { mensaje: 'Usuario eliminado' };
+export async function eliminarUsuario(id) {
+  await pool.query("DELETE FROM usuarios WHERE id = ?", [id]);
+  return { mensaje: "Usuario eliminado" };
 }
+/* {
+  "nombre": "Juan Perez",
+  "email": "juan3@example.com",
+  "fecha_nacimiento": "1990-05-22",
+  "estado": "activo"
+} */
