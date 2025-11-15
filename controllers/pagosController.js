f1886f82d072ab09a2c9865f042d46311f6eb104
mikeAbril <jaimecarreno734@gmail.com>
2025-11-15 16:45:23 -0500

    falta mirar la busqueda por ID y Actualizar el pago, y ya se pueden hacer pagos y mirar los que hay

--- controllers/pagosController.js
+++ controllers/pagosController.js
@@ -11,15 +11,31 @@ export async function getPagos(req, res) {
 }
 
 export async function getPago(req, res) {
+    try {
     const pago = await obtenerPagoPorId(req.params.id);
-    if(!pago) return res.status(404).json({error: 'Pago no encontrado'});
+    if(!pago) return res.status(404).json({error: 'Pago no encontrado, verifica si existe'});
     res.json(pago);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener pago' });
+    }
+    
 }
 
 export async function postPago(req, res) {
-    const nuevo = await crearPago(req.body);
-    res.status(201).json(nuevo);
+  try {
+    const { usuario_id, monto, metodo } = req.body;
+
+    const nuevoPago = await crearPago(usuario_id, monto, metodo);
+
+    res.status(201).json(nuevoPago);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al crear un Nuevo Pago" });
+  }
 }
+
+
 
 export async function getPagoEstado(req, res) {
     const pagoActualizado = await actualizarPago(req.params.id, req.body);
--- models/pagosModel.js
+++ models/pagosModel.js
@@ -6,16 +6,27 @@ export async function obtenerPagos() {
 }
 
 export async function obtenerPagoPorId(id) {
-  const [rows] = await pool.query('SELECT * FROM pagos WHERE id = ?', [id]);
+  const [rows] = await pool.query('SELECT * FROM pagos WHERE usuario_id = ?', [id]);
   return rows[0];
 }
 
 
-export async function crearPago(id, { usuario_id, monto, metodo }) {
-  await pool.query('UPDATE pagos SET usuario_id = ?, monto = ?, metodo = ? WHERE id = ?', 
-                   [usuario_id, monto, metodo, id]);
-  return { id, usuario_id, monto, metodo };
+export async function crearPago(usuario_id, monto, metodo ) {
+  const [result] = await pool.query(
+    `INSERT INTO pagos(usuario_id, monto, fecha_pago, fecha_vencimiento, metodo)
+     VALUES(?, ?, CURDATE(), DATE_ADD(CURDATE(), INTERVAL 30 DAY), ?)`,
+    [usuario_id, monto, metodo]
+  );
+  
+  return {
+    id: result.insertId,
+    usuario_id,
+    monto,
+    metodo,
+    fechaPago: new Date()
+  };
 }
+
 
 
 export async function actualizarPago(id, { monto, metodo }) {
